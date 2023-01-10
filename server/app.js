395654b952c840f1d1cadfb0b6cadf60b9fedc6c
Terel <terel.phillips23@gmail.com>
2023-01-10 13:41:55 -0500

    -deleted src folder in server directory -moved contents to root -added vercel.json file -created start commad in package.json

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const connectDB = require('../config/db');
+const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
@@ -28,27 +28,27 @@ app.get('/', (req, res) => res.send('API Running'));
 // Define Routes
 
 // Authentication
-app.use('/api/auth', require('../routers/userRouter'));
+app.use('/api/auth', require('./routers/userRouter'));
 
 // Users
 // app.use('/api/users', require('../routers/users'));
 
 // Categories
-app.use('/api/categories', require('../routers/categoriesRouter'));
+app.use('/api/categories', require('./routers/categoriesRouter'));
 
 // Products
-app.use('/api/products', require('../routers/productsRouter'));
+app.use('/api/products', require('./routers/productsRouter'));
 
 // product
-app.use('/api/product', require('../routers/productRouter'));
+app.use('/api/product', require('./routers/productRouter'));
 
 // Collections
-app.use('/api/collections', require('../routers/collectionRouter'));
+app.use('/api/collections', require('./routers/collectionRouter'));
 
 // Reviews
-app.use('/api/reviews', require('../routers/reviewsRouter'));
+app.use('/api/reviews', require('./routers/reviewsRouter'));
 
 // Payments
-app.use('/api/payment', require('../routers/stripeRouter'));
+app.use('/api/payment', require('./routers/stripeRouter'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
