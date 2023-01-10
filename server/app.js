9ebfa03d1303a72384b99df61cb15258020b9b87
Terel <terel.phillips23@gmail.com>
2023-01-10 14:32:02 -0500

    -created environment variable for frontend

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.json({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
