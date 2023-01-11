2c9a1dfa6302d5c9022d81515085b5f0b3b528a8
Terel <terel.phillips23@gmail.com>
2023-01-11 01:34:34 -0500

    -changes

--- server/app.js
+++ server/app.js
@@ -16,18 +16,6 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return res.status(200).json({});
-  }
-  next();
-});
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
