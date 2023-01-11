454f5e8ba76c2967e13d1eefa6f5dca436aa98f0
Terel <terel.phillips23@gmail.com>
2023-01-10 21:41:31 -0500

    -updated handleLoggedIn function

--- server/controllers/loggedIn.js
+++ server/controllers/loggedIn.js
@@ -1,11 +1,16 @@
 const handleLoggedIn = (req, res, jwt) => {
   try {
+    // get the token from the request
     const token = req.cookies.token;
-    if (!token) return res.json(false);
-    jwt.verify(token, process.env.JWT_SECRET);
-    res.send(true);
+    if (!token) return res.json(false); // if there is no token return false
+    // verify the token
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) return res.json(false); // if token is not valid return false
+      return res.json(true); // if token is valid return true
+    });
   } catch (err) {
-    res.json(false);
+    console.error(err.message);
+    res.status(500).send('Server error');
   }
 };
 
