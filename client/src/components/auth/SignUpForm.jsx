f04f9c1adf7fb7201bc1a455bcc153c5060c310a
Terel <terel.phillips23@gmail.com>
2023-01-17 02:43:59 -0500

    -created detailed readme file for project

--- client/src/components/auth/SignUpForm.jsx
+++ client/src/components/auth/SignUpForm.jsx
@@ -1,9 +1,10 @@
+import { AuthContext } from '../../context/AuthContext';
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import FormButton from '../form/FormButton';
 import FormLabel from '../form/FormLabel';
-import { useContext } from 'react';
-import { AuthContext } from '../../context/AuthContext';
+import Layout from '../Layout';
 
 const SignUpForm = () => {
   const [firstName, setFirstName] = useState('');
@@ -36,65 +37,67 @@ const SignUpForm = () => {
   };
 
   return (
-    <form
-      onSubmit={signUp}
-      className="flex flex-col gap-10 justify-center items-center mt-10"
-    >
-      {errors && <span className="text-red-500">{errors}</span>}
-      <FormLabel
-        label="First Name"
-        inputType="text"
-        inputName="firstName"
-        value={firstName}
-        onChange={e => setFirstName(e.target.value)}
-      />
-      <FormLabel
-        label="Last Name"
-        inputType="text"
-        inputName="lastName"
-        value={lastName}
-        onChange={e => setLastName(e.target.value)}
-      />
-      <FormLabel
-        label="Username"
-        inputType="text"
-        inputName="username"
-        value={username}
-        onChange={e => setUsername(e.target.value)}
-      />
-      <FormLabel
-        label="Email"
-        inputType="email"
-        inputName="email"
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-      />
-      <FormLabel
-        label="Password"
-        inputType="password"
-        inputName="password"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-      />
-      <FormLabel
-        label="Confirm Password"
-        inputType="password"
-        inputName="passwordVerify"
-        value={passwordVerify}
-        onChange={e => setPasswordVerify(e.target.value)}
-      />
-      <p>
-        Already have an account?{' '}
-        <span
-          onClick={toSignIn}
-          className="text-blue-500 cursor-pointer hover:text-blue-900 transition-all duration-500"
-        >
-          Sign In
-        </span>
-        .
-      </p>
-      <FormButton label="Sign Up" />
-    </form>
+    <Layout>
+      <form
+        onSubmit={signUp}
+        className="flex flex-col gap-10 justify-center items-center mt-10"
+      >
+        {errors && <span className="text-red-500">{errors}</span>}
+        <FormLabel
+          label="First Name"
+          inputType="text"
+          inputName="firstName"
+          value={firstName}
+          onChange={e => setFirstName(e.target.value)}
+        />
+        <FormLabel
+          label="Last Name"
+          inputType="text"
+          inputName="lastName"
+          value={lastName}
+          onChange={e => setLastName(e.target.value)}
+        />
+        <FormLabel
+          label="Username"
+          inputType="text"
+          inputName="username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <FormLabel
+          label="Email"
+          inputType="email"
+          inputName="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <FormLabel
+          label="Password"
+          inputType="password"
+          inputName="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <FormLabel
+          label="Confirm Password"
+          inputType="password"
+          inputName="passwordVerify"
+          value={passwordVerify}
+          onChange={e => setPasswordVerify(e.target.value)}
+        />
+        <p>
+          Already have an account?{' '}
+          <span
+            onClick={toSignIn}
+            className="text-blue-500 cursor-pointer hover:text-blue-900 transition-all duration-500"
+          >
+            Sign In
+          </span>
+          .
+        </p>
+        <FormButton label="Sign Up" />
+      </form>
+    </Layout>
   );
 };
 
---
+++ readme.md
@@ -0,0 +1,102 @@
+# AVANT - FULL-STACK
+
+This is a Full-stack e-commerce web application
+
+The goal was to build an authentication system using React and JWT on the client side and Express on the server side. The process of building this system involved several steps, including setting up the client and server, creating the necessary routes and functions, and implementing the use of JWT.
+
+Landing Page
+
+![alt text](https://im.ezgif.com/tmp/ezgif-1-841d016dd8.gif)
+
+Home Page
+
+![alt text](avant%20gif.gif)
+
+## Technologies Used
+
+- React
+- TailwindCSS
+- SwiperJS
+- Yup & Formik
+- Axios
+- Node.js
+- Express
+- JWT (JSON Web Tokens)
+- bcrypt
+
+# Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+## Prerequisites
+
+- Node.js
+- npm
+- React
+- Axios
+- Node.js
+- Express
+- JWT (JSON Web Tokens)
+- bcrypt
+
+## Installing
+
+### Clone the repository
+
+```
+git clone git@github.com:UziStacks/avant-fullstack.git
+```
+
+## Frontend - client
+
+### Install the dependencies
+
+```
+npm i
+```
+
+### Start the development server
+
+```
+npm run start
+```
+
+### Open http://localhost:3000 to view the app in the browser.
+
+## Backend - server
+
+### Install the dependencies
+
+```
+npm install
+```
+
+### Start the development server
+
+```
+npm start
+```
+
+### Open http://localhost:5000 to view the app in the browser.
+
+## Functionality
+
+- fully functional cart system
+- functioning checkout process
+- User can create an account
+- User can login
+- User can perform certain authenticated actions
+
+## Challenges and Solutions
+
+- Implementing JWT authentication: To solve this, I used the jsonwebtoken library to handle the generation and verification of tokens on the server side and the use of tokens for authenticated requests on the client side.
+
+## Future Improvements
+
+- Add forgot password functionality
+- Add email verification feature
+- change name, username, password, email, and profile picture
+
+## Authors
+
+- Terel Phillips - @Uzistacks
